fc0e91df0588efaac6b23ea0292372384f05fa5d
wyq <yaqiang.wang@gmail.com>
2020-09-28 13:10:44 +0800

    change PathUtil class to SystemUtils

---
+++ MeteoInfoLib/src/main/java/org/meteoinfo/util/SystemUtils.java
@@ -0,0 +1,57 @@
+package org.meteoinfo.util;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+public class SystemUtils {
+    /**
+     * Sets the java library path to the specified path
+     *
+     * @param path the new library path
+     * @throws Exception
+     */
+    public static void setLibraryPath(String path) throws Exception {
+        String lib = System.getProperty("java.library.path");
+        path = path + ";" + lib;
+        System.setProperty("java.library.path", path);
+
+        //set sys_paths to null
+        try {
+            final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+            sysPathsField.setAccessible(true);
+            sysPathsField.set(null, null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Adds the specified path to the java library path
+     *
+     * @param pathToAdd the path to add
+     * @throws Exception
+     */
+    public static void addLibraryPath(String pathToAdd) throws Exception{
+        try {
+            final Field usrPathsField = ClassLoader.class.getDeclaredField("usr_paths");
+            usrPathsField.setAccessible(true);
+
+            //get array of paths
+            final String[] paths = (String[]) usrPathsField.get(null);
+
+            //check if the path to add is already present
+            for (String path : paths) {
+                if (path.equals(pathToAdd)) {
+                    return;
+                }
+            }
+
+            //add the new path
+            final String[] newPaths = Arrays.copyOf(paths, paths.length + 1);
+            newPaths[newPaths.length - 1] = pathToAdd;
+            usrPathsField.set(null, newPaths);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+}

a689ec79bc9e987760d8e160a5ca314d6a768fcc
wyq <yaqiang.wang@gmail.com>
2021-05-08 14:58:51 +0800

    add MathEx class

---
+++ meteoinfo-math/src/main/java/org/meteoinfo/math/MathEx.java
@@ -0,0 +1,12 @@
+package org.meteoinfo.math;
+
+public class MathEx {
+    /**
+     * Returns x * x.
+     * @param x a real number.
+     * @return the square of x.
+     */
+    public static double pow2(double x) {
+        return x * x;
+    }
+}

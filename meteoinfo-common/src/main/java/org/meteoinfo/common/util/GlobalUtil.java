973026df64df692b28e166517cfb8e3d549c9643
wyq <yaqiang.wang@gmail.com>
2022-08-30 23:11:08 +0800

    to version 3.4.2

--- meteoinfo-common/src/main/java/org/meteoinfo/common/util/GlobalUtil.java
+++ meteoinfo-common/src/main/java/org/meteoinfo/common/util/GlobalUtil.java
@@ -67,7 +67,7 @@ import java.util.zip.ZipInputStream;
      public static String getVersion(){
          String version = GlobalUtil.class.getPackage().getImplementationVersion();
          if (version == null || version.equals("")) {
-             version = "3.4.1";
+             version = "3.4.2";
          }
          return version;
      }
